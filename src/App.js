e2d855ccca89d935239ee947be62ab6c1ebaf9ef
saravanasai <saravanasai002@gmail.com>
2022-03-02 22:22:17 +0530

    route setup partial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Layout from "./Layout/Layout"
+import routes from "./routes"
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div className="container py-3">
+    <Routes>
+    {routes.map(route => {
+          return <Route path={route.path} element={<>heloo</>} />
+        })}
+    </Routes>
+        <Header />
+        <main>
+          <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+           
+          </div>
+        </main>
+      </div>
+    
+      
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Product.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Product = () => {
+    return ( <>
+    
+      Product Page
+    </> );
+}
+ 
+export default Product;
